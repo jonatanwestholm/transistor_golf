9727974babf1d9c2783b3cafbbdc88d300730325
jonatanwestholm <jonatanwestholm@gmail.com>
2020-09-18 22:17:48 +0200

    working python backend for 'not gate'

--- circuit.py
+++ circuit.py
@@ -1,12 +1,14 @@
+from itertools import groupby, chain
+from functools import partial
 from collections import defaultdict, namedtuple
 import numpy as np
 import networkx as nx
 
 from sugarrush.solver import SugarRush
 
-Bar = namedtuple("Bar", ["comp_type", "pos", "rot", "length"])
-Transistor = namedtuple("Transistor", ["comp_type", "pos", "rot"])
-Node = namedtuple("Node", ["comp_type", "pos", "signal_type"])
+Bar = namedtuple("Bar", ["pos", "rot", "length"])
+#Transistor = namedtuple("Transistor", ["comp_type", "pos", "rot", "var"])
+Node = namedtuple("Node", ["name", "pos", "var"])
 
 '''
     if component.comp_type == "Node":
@@ -23,17 +25,16 @@ Node = namedtuple("Node", ["comp_type", "pos", "signal_type"])
         return [component.pos + np.array([i, j]) for i, j in diffs]
 '''
 
-def get_covered_points(component):
-    if component.comp_type == "Bar":
-        if component.rot == 0:
+def get_covered_points(bar):
+        if bar.rot == 0:
             diff = (1, 0)
-        elif component.rot == 1:
+        elif bar.rot == 1:
             diff = (0, 1)
-        elif component.rot == 2:
+        elif bar.rot == 2:
             diff = (-1, 0)
-        elif component.rot == 3:
+        elif bar.rot == 3:
             diff = (0, -1)
-        return [tuple(np.array(component.pos) + i * np.array(diff)) for i in range(component.length)]
+        return [tuple(np.array(bar.pos) + i * np.array(diff)) for i in range(bar.length)]
 
 
 def get_connected_components(bars):
@@ -54,26 +55,115 @@ def get_connected_components(bars):
         for coord in coords:
             G.add_edge(comp, coord)
 
-    bar_ex = next(iter(bars))
+    #bar_ex = next(iter(bars))
+    coord_ex = next(iter(coords))
     for cc in nx.connected_components(G):
-        yield [c for c in cc if type(c) == type(bar_ex)]
+        #yield [c for c in cc if type(c) == type(bar_ex)]
+        yield [c for c in cc if type(c) == type(coord_ex)]
 
 
-# given components, return satisfiable assignments to all nodes
-def get_satisfiable():
-    solver = SugarRush()
+def get_coord_dict(bars):
+    ccs = get_connected_components(bars)
+    coord_dict = {}
+    for cc in ccs:
+        for c in cc:
+            coord_dict[c] = cc[0]
+    return coord_dict
+
+
+def get_NOT_GATE(solver):
+    bars = [Bar((0, 0), 1, 5),
+            Bar((0, 1), 0, 5),
+            Bar((0, 3), 0, 5)
+        ]
+
+    supplys = [Node("VDD", (5, 0), solver.var())]
+    grounds = [Node("GND", (5, 4), solver.var())]
+    inputs  = [Node("X", (0, 2), solver.var())]
+    outputs = [Node("T", (5, 2), solver.var())]
+
+    transistors = [[Node("POS_G", (4, 1), solver.var()), 
+                    Node("POS_S", (5, 2), solver.var()),
+                    Node("POS_D", (5, 0), solver.var()), -1],
+                   [Node("NEG_G", (4, 3), solver.var()), 
+                    Node("NEG_S", (5, 4), solver.var()),
+                    Node("NEG_D", (5, 2), solver.var()), 1],
+                ]
+
+    return bars, supplys, grounds, inputs, outputs, transistors
 
 
 def main():
-    bars = [Bar("Bar", (0, 0), 0, 5),
-            Bar("Bar", (4, 0), 0, 5),
-            Bar("Bar", (7, 0), 1, 5),
-            Bar("Bar", (7, 5), 0, 5)
+    solver = SugarRush()
+
+    '''
+    bars = [Bar("Bar", (0, 0), 0, 5, solver.var()),
+            Bar("Bar", (4, 0), 0, 5, solver.var()),
+            Bar("Bar", (7, 0), 1, 5, solver.var()),
+            Bar("Bar", (7, 5), 0, 5, solver.var())
         ]
+    supplys = [Node("Node", (0, 0), "supply", solver.var())]
+    grounds = [Node("Node", (8, 5), "ground", solver.var())]
+    inputs  = [Node("Node", (7, 0), "input", solver.var())]
+    outputs = []
+    transistors = []
+    '''
+    bars, supplys, grounds, inputs, outputs, transistors = get_NOT_GATE(solver)
+
+    all_vars = supplys + grounds + inputs + outputs + [node for trans in transistors for node in trans[:-1]]
+    #print(all_vars)
+    coord_dict = get_coord_dict(bars)
+    #print(coord_dict)
+
+    def default_map(c):
+        if c.pos in coord_dict:
+            return coord_dict[c.pos]
+        else:
+            return c.pos
+
+    ccs = []
+    for _, cc in groupby(sorted(all_vars, key=default_map), key=default_map):
+        cc = list(cc)
+        all_on = [[c.var] for c in cc]
+        all_off = [[-c.var] for c in cc]
+        #print(all_on, all_off)
+        solver.add(solver.disjunction([all_on, all_off]))
+        print(", ".join([c.name for c in cc]))
+        ccs.append(cc)
+
+    for supply in supplys:
+        solver.add([supply.var])
+
+    for ground in grounds:
+        solver.add([-ground.var])
+
+    for gate, source, drain, trans_sign in transistors:
+        solver.add(get_transistor_clauses(gate.var, source.var, drain.var, trans_sign))
+
+    x_lit = inputs[0].var
+    t_lit = outputs[0].var
+
+    for x_sign in [-1, 1]:
+        if solver.solve(assumptions=[x_sign * x_lit]):
+            print("x:", solver.solution_value(x_lit), ", t:", solver.solution_value(t_lit))
+            for cc in ccs:
+                print(*[solver.solution_value(c.var) for c in cc])
+        else:
+            print("assumption failed:", x_sign)
+    '''
+    '''
 
 
-    for connected_component in get_connected_components(bars):
-        print(connected_component)
+def get_transistor_clauses(gate, source, drain, trans_sign):
+    # trans_sign: 1 for positive transistor (connects when gate is supply voltage)
+    #            -1 for negative transistor (connects when gate is  drain voltage)
+    return [[-trans_sign * gate, source, -drain], [-trans_sign * gate, -source, drain]]
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
